7191c6592b687301ac426565d0090c72784ed1d7
firework72 <fir3work72@gmail.com>
2026-01-29 11:47:59 +0900

    Dong : 주식 상세 페이지에 개수 입력란과 매수/매도 버튼 추가
    Introduced input and buttons for stock buy/sell actions in StockDetail.jsx, and updated the CSS for better layout and centering. Also added getStockName API method and improved stock price fetching logic to update every 10 seconds.

--- doyouknowju-frontend/src/components/features/stock/StockDetail.jsx
+++ doyouknowju-frontend/src/components/features/stock/StockDetail.jsx
@@ -2,6 +2,7 @@ import styles from './StockDetail.module.css';
 import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import { tradeApi } from '../../../api/trade/TradeApi.js';
+import {Button, Input} from '@/components/common';
 /*
     필요한 상태값 : 주식 ID, 회원 정보
     필요한 함수 : 주식 매수, 주식 매도
@@ -20,6 +21,10 @@ function StockDetail() {
     };
 
     useEffect(() => {
+        // 마운트 시 초기 1회 주식 현재가 정보 조회
+        fetchStockPrice();
+
+        // 이후 10초마다 주식 현재가 정보 갱신
         setInterval(() => {
             fetchStockPrice();
         }, 10000);
@@ -37,6 +42,23 @@ function StockDetail() {
             <h1>StockDetail</h1>
             <span>{stockId}</span>
             <h3>{stockPrice}</h3>
+            <Input
+                type="number"
+                placeholder="수량"
+                disabled={stockPrice === 0}
+            />
+            <Button
+                variant="danger"
+                disabled={stockPrice === 0}
+            >
+                매수
+            </Button>
+            <Button
+                variant="primary"
+                disabled={stockPrice === 0}
+            >
+                매도
+            </Button>  
         </div>
     );
 }
